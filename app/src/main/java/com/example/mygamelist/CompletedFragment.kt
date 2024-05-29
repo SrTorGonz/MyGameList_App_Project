c3244c735f18fbd3c1b70daef4bc41bfc00c403a
Daniel Torres <yunior1941@gmail.com>
2024-05-29 18:39:00 -0500

    Playing y Completed terminados

--- app/src/main/java/com/example/mygamelist/CompletedFragment.kt
+++ app/src/main/java/com/example/mygamelist/CompletedFragment.kt
@@ -1,35 +1,30 @@
 package com.example.mygamelist.ui.notifications
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.mygamelist.GamesFirebase
-import com.example.mygamelist.R
 import com.example.mygamelist.adapter.PlayingAdapter
-import com.example.mygamelist.databinding.FragmentMylistBinding
-import com.example.mygamelist.ui.dashboard.SearchFragmentDirections
+import com.example.mygamelist.databinding.FragmentCompletedBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.EventListener
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.QuerySnapshot
-import com.google.firebase.firestore.toObject
 
-class MyListFragment : Fragment() {
+class CompletedFragment : Fragment() {
 
-    private lateinit var recyclerView:RecyclerView
     private lateinit var gameArrayList:ArrayList<GamesFirebase>
-    private lateinit var myAdapter:PlayingAdapter
+    private lateinit var myAdapter: PlayingAdapter
     private lateinit var db: FirebaseFirestore
 
-    private var _binding: FragmentMylistBinding? = null
+    private var _binding: FragmentCompletedBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -41,9 +36,13 @@ class MyListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentMylistBinding.inflate(inflater, container, false)
+        _binding = FragmentCompletedBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        binding.titlePlaying.setOnClickListener{
+            val action = CompletedFragmentDirections.actionNavigationCompletedToNavigationMylist()
+            findNavController().navigate(action)
+        }
 
         binding.recyclerPlaying.layoutManager = LinearLayoutManager(context)
         binding.recyclerPlaying.setHasFixedSize(true)
@@ -55,11 +54,6 @@ class MyListFragment : Fragment() {
 
         eventChangeListener()
 
-        binding.titleCompleted.setOnClickListener{
-            val action = MyListFragmentDirections.actionNavigationMylistToCompletedFragment()
-            findNavController().navigate(action)
-        }
-
         return root
     }
 
@@ -67,14 +61,14 @@ class MyListFragment : Fragment() {
     {
         db=FirebaseFirestore.getInstance()
         val userEmail = FirebaseAuth.getInstance().currentUser?.email ?: return
-        db.collection("users").document(userEmail).collection("Playing")
+        db.collection("users").document(userEmail).collection("Completed")
             .addSnapshotListener(object :EventListener<QuerySnapshot>{
                 override fun onEvent(
                     value: QuerySnapshot?,
                     error: FirebaseFirestoreException?
                 ) {
                     if (error !=null){
-                        Log.e("Firestore Error",error.message.toString())
+                        Toast.makeText(requireContext(), "Nothing to show", Toast.LENGTH_SHORT).show()
                         return
                     }
 
--- app/src/main/java/com/example/mygamelist/GameFragment.kt
+++ app/src/main/java/com/example/mygamelist/GameFragment.kt
@@ -1,6 +1,5 @@
 package com.example.mygamelist
 
-import android.R
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
