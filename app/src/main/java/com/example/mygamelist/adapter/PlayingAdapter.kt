f0475154775f208e5f89b7fec8104d7dc02bd33c
Daniel Torres <yunior1941@gmail.com>
2024-05-29 17:01:06 -0500

    imagen en mylist

--- app/src/main/java/com/example/mygamelist/adapter/PlayingAdapter.kt
+++ app/src/main/java/com/example/mygamelist/adapter/PlayingAdapter.kt
@@ -3,8 +3,10 @@ package com.example.mygamelist.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.mygamelist.GamesFirebase
 import com.example.mygamelist.R
 
@@ -20,6 +22,10 @@ class PlayingAdapter (private val gameList:ArrayList<GamesFirebase> ): RecyclerV
         holder.gameName.text= game.nombre
         holder.score.text=game.ratingP.toString()
         holder.review.text=game.comment
+
+        Glide.with(holder.itemView.context)
+            .load(game.imagenPortada)
+            .into(holder.imagenPortada)
     }
 
     override fun getItemCount(): Int {
@@ -31,7 +37,7 @@ class PlayingAdapter (private val gameList:ArrayList<GamesFirebase> ): RecyclerV
         val gameName:TextView=itemView.findViewById(R.id.text_gameName)
         val score:TextView=itemView.findViewById(R.id.text_userScore)
         val review:TextView=itemView.findViewById(R.id.text_review)
-
+        val imagenPortada:ImageView=itemView.findViewById(R.id.img_portada)
 
     }
 }
